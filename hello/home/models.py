78f1dbb8e9c68ad3065b02b156631a3b1063f969
Shraddha <shraddha29mane@gmail.com>
2022-06-08 17:10:52 +0530

    'commit'

---
+++ hello/home/models.py
@@ -0,0 +1,11 @@
+from tkinter.messagebox import YES
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=122)
+    email=models.CharField(max_length=122)
+    phone=models.CharField(max_length=10)
+    desc=models.TextField()
+    date=models.DateField()
+    
